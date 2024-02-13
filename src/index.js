95922b340862e784a0f9746d3c5fe9bc1e9f38be
Sergio Jauregui <sergioja2003@gmail.com>
2024-02-13 11:47:48 -0500

    Portafolio v1 - Header

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM        from 'react-dom/client';
+import Root            from './Components/Root.js';
+import ContainerInfo   from './Components/ContainerInfo.js';
+import React           from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
+const containerInfo = ReactDOM.createRoot(document.getElementById('containerInfo'));
+containerInfo.render(
+  <React.StrictMode>
+    <ContainerInfo/>
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
